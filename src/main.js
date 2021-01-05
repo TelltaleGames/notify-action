63e0a68db730b5c89729ca0edd1d6895a061e84f
Indy Ray <arelius@gmail.com>
2021-01-04 20:43:23 -0800

    Initial notify-action commit.

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch');
+const mustache = require('mustache');
+
+// Environment seems to be where everything useful is passed in, perhaps process everything starting with INPUT_?
+const notification_data = process.env;
+
+async function notify(notification) {
+    try {
+        const method = notification.method || 'POST';
+        const headers = notification.headers || { 'Content-Type': 'application/json' }
+        const response = await fetch(notification.url, {
+            method: method,
+            body: mustache.render(notification.body, notification_data),
+            headers: headers
+        });
+        console.log(mustache.render(notification.body, notification_data));
+        if(!response.ok) {
+            throw Error(response.statusText);
+        }
+    } catch(error) {
+        core.setFailed(error.message);
+    }
+}
+
+try {
+    const notifications_text = core.getInput('notifications');
+    const notifications = JSON.parse(notifications_text);
+    for(const notification of notifications.notifications) {
+        notify(notification);
+    }
+} catch(error) {
+    core.setFailed(error.message);
+}
